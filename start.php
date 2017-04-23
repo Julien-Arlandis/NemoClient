45c1a5acc0d64302965c4372253673535b8d9b77
Julien Arlandis <julien.arlandis@gmail.com>
2017-04-23 09:06:58 +0200

    Delete start.php

--- start.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once( __DIR__."/lib/class.jntp.php" );
-if( !file_exists( __DIR__ . '/conf/config.json' ) ) { require_once( __DIR__."/install/install.php" ); die(); }
-if( file_exists( __DIR__ . '/sleep' ) ) { die( 'You must remove sleep file to continue<br><strong>rm jntp/sleep</strong>' ); }
